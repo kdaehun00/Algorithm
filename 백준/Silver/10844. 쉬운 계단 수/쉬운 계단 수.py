25644d5740b8949185f9c29b004cd44a0c6fb9ea
kdaehun00 <kdaehun00@naver.com>
2025-07-30 01:46:35 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+MOD = 10**9
+N = int(input())
+
+dp = [[0]*10 for _ in range(N+1)]
+
+for i in range(1, 10):
+    dp[1][i] = 1
+
+for n in range(2, N+1):
+    for k in range(10):
+        if k > 0:
+            dp[n][k] += dp[n-1][k-1]
+        if k < 9:
+            dp[n][k] += dp[n-1][k+1]
+        dp[n][k] %= MOD
+
+print(sum(dp[N]) % MOD)
