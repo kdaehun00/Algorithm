e570a770106009c7cf41b789a4196327006e94f9
kdaehun00 <kdaehun00@naver.com>
2025-08-04 10:14:01 +0900

    [Silver III] Title: 퇴사, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -0,0 +1,22 @@
+import sys
+
+input = sys.stdin.readline
+N = int(input())
+
+T = []
+P = []
+
+for _ in range(N):
+  t, p = map(int, input().split())
+  T.append(t)
+  P.append(p)
+
+dp = [0] * (N+1)
+
+for i in range(N - 1, -1, -1):
+  if i + T[i] <= N:
+    dp[i] = max(dp[i+1], dp[i + T[i]] + P[i])
+  else:
+    dp[i] = dp[i+1]
+
+print(dp[0])
