0b1ff5ce8345a171cd3c53306d1f3f69a9c30be8
kdaehun00 <kdaehun00@naver.com>
2025-09-18 00:09:17 +0900

    [level 1] Title: 실패율, Time: 2777.83 ms, Memory: 17.2 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,15 +1,19 @@
 def solution(N, stages):
 
     users = len(stages)
-    fail_rate_list = [0 for _ in range(N+1)]
+    fail_rate_list = []
     
     for i in range(1, N+1):
         if i in stages:
             fail = stages.count(i)
-            fail_rate_list[i] = fail / users
+            fail_rate_list.append((i, fail / users))
             users -= fail
             stages.remove(i)
-    
-    answer = [i for i, v in sorted(enumerate(fail_rate_list[1:], start=1), key=lambda x: -x[1])]
+        else:
+            fail_rate_list.append((i, 0))
 
+    
+    fail_rate_list.sort(key= lambda x: (-x[1], x[0]))
+    
+    answer = [stage for stage, rate in fail_rate_list]
     return answer
