dd06c1de126e284850571d4375e2e41b2c17f987
kdaehun00 <kdaehun00@naver.com>
2025-09-22 23:33:03 +0900

    [level 1] Title: 신고 결과 받기, Time: 1355.38 ms, Memory: 38.5 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,27 +1,18 @@
 from collections import defaultdict
 
 def solution(id_list, report, k):
-    answer = []
-    block_list = []
+    answer = [0] * len(id_list)
     db = defaultdict(int)
-    result_db = defaultdict(list)
     
     set_report = list(set(report))
     
     for data in set_report:
         a, b = data.split()
         db[b] += 1
-        result_db[a].append(b)
     
-    for key, value in db.items():
-        if value >= k:
-            block_list.append(key)
-    
-    for user in id_list:
-        count = 0
-        for block in result_db[user]:
-            if db[block] >= k:
-                count+=1
-        answer.append(count)
-        
+    for data in set_report:
+        a, b = data.split()
+        if db[b] >= k:
+            answer[id_list.index(a)] += 1
+            
     return answer
