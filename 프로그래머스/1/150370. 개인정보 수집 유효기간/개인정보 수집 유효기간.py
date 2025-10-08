8bd78a7b68a1c762757be3917847d3d5876ac02c
kdaehun00 <kdaehun00@naver.com>
2025-10-08 23:21:19 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.23 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,26 +1,26 @@
-"""
-A - 6, B - 12, C - 3
-
-출력: 파기해야할 개인정보 번호
-"""
 from collections import defaultdict
+
 def solution(today, terms, privacies):
-    def to_days(date_str):
-        y, m, d = map(int, date_str.split("."))
-        return y*12*28 + m*28 + d
-    
-    today_days = to_days(today)
-    db = defaultdict(int)
-    
-    for term in terms:
-        data_type, data_period = term.split()
-        db[data_type] = int(data_period)
-    
     answer = []
-    for i in range(len(privacies)):
-        date, data_type = privacies[i].split()
-        days = to_days(date) + (db[data_type] * 28)
-        if days <= today_days:
+    db = defaultdict(int)
+    for term in terms:
+        category, period = term.split()
+        db[category] = int(period)
+    
+    def convert_date(date):
+        y, m, d = date.split(".")
+        return 28*(12*int(y) + int(m)) + int(d)
+    
+    today = convert_date(today)
+    
+    def convert_end_date(date, period):
+        return convert_date(date) + period*28
+        
+    for i, privacy in enumerate(privacies):
+        date, category = privacy.split()
+        end_date = convert_end_date(date, db[category])
+        
+        if end_date <= today:
             answer.append(i+1)
         
     return answer
