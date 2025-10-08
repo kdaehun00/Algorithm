fa608591612779d90435136dc7c2f3abefe7915f
kdaehun00 <kdaehun00@naver.com>
2025-10-09 00:29:08 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.06 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -1,41 +1,40 @@
 """
-아이디: 3자 이상 15자 이하 (알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.) 사용가능)
-단, 마침표(.)는 처음과 끝에 사용할 수 없으며 또한 연속으로 사용할 수 없습니다.
-
 소문자 치환
-문자 제거
-연속 .를 하나로 치환
-.이 처음이나 끝에 있으면 제거
+-, _, . 빼고 모두 제거
+.. -> . 로 치환
+앞뒤 . 제거
 빈 문자열이면 a 대입
-16글자면 뒤에 제거, 끝에 마침표 있으면 제거
-마지막 글자 반복으로 3글자 이상 만들기
+16글자 이상이면 15글자로 정리 후 앞뒤 .제거
+2글자 이하면 끝문자 반복 3이상일 때까지
 """
 def solution(new_id):
     answer = ''
+    can_not = "~!@#$%^&*()=+[{]}:?,<>/"
+    
+    # 1단계
     new_id = new_id.lower()
-    disable_list = [
-        '~', '!', '@', '#', '$', '%', '^', '&', '*', 
-        '(', ')', '=', '+', '[', '{', ']', '}', 
-        ':', '?', ',', '<', '>', '/'
-    ]    
-
-    for disable_word in disable_list:
-        new_id = new_id.replace(disable_word, '')
     
-    while '..' in new_id:
-        new_id = new_id.replace('..', '.')
+    # 2단계
+    for word in can_not:
+        new_id = new_id.replace(word, "")
     
-    new_id = new_id.strip('.')
+    # 3단계
+    while ".." in new_id:
+        new_id = new_id.replace("..", ".")
     
-    if not new_id:
-        new_id = "a"
+    # 4단계
+    new_id = new_id.strip(".")
     
+    # 5단계
+    if len(new_id) <= 0:
+        new_id += 'a'
+    
+    # 6단계
     if len(new_id) >= 16:
-        new_id = new_id[:15]
-    
-    new_id = new_id.strip('.')
-    
-    while len(new_id) < 3:
+        new_id = new_id[:15].rstrip(".")
+        
+    # 7단계
+    while len(new_id) <= 2:
         new_id += new_id[-1]
         
     return new_id
