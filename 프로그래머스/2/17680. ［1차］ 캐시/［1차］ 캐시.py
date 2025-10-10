3fc5c015d759beb1d2de4171243959d56bd22c7b
kdaehun00 <kdaehun00@naver.com>
2025-10-10 21:54:02 +0900

    [level 2] Title: [1차] 캐시, Time: 100.96 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+def solution(cacheSize, cities):
+    answer = 0
+    cache_data = deque()
+    if not cacheSize:
+        return 5*len(cities)
+    
+    for city in cities:
+        city = city.lower()
+        if city in cache_data:
+            cache_data.remove(city)
+            cache_data.appendleft(city)
+            answer += 1
+        else:
+            if len(cache_data) >= cacheSize:
+                cache_data.pop()
+            cache_data.appendleft(city)
+            answer += 5
+    return answer
