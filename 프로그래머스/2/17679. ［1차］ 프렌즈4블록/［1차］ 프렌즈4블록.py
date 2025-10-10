dc31441fe4eb4d2ede0fff0a7599b01914af7547
kdaehun00 <kdaehun00@naver.com>
2025-10-10 22:30:44 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 56.14 ms, Memory: 9.33 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
@@ -1,29 +1,30 @@
+from collections import deque
+
 def solution(m, n, board):
-    count = 0
+    answer = 0
     board = [list(data) for data in board]
     
     while True:
         remove = set()
-        
         for y in range(m-1):
             for x in range(n-1):
-                if board[y][x] != ' ' and board[y][x] == board[y][x+1] == board[y+1][x] == board[y+1][x+1]:
-                    remove.add((y, x))
-                    remove.add((y, x+1))
-                    remove.add((y+1, x))
-                    remove.add((y+1, x+1))
+                if board[y][x] != " " and board[y][x] == board[y][x+1] == board[y+1][x] == board[y+1][x+1]:
+                    remove.update({(y, x), (y, x+1), (y+1, x), (y+1, x+1)})
+                    
         if not remove:
             break
             
         for y, x in remove:
-            board[y][x] = ' '
+            board[y][x] = " "
         
-        count += len(remove)
+        answer += len(remove)
         
         for x in range(n):
-            stack = [board[y][x] for y in range(m) if board[y][x] != ' ']
-            
+            move_list = deque()
             for y in range(m-1, -1, -1):
-                board[y][x] = stack.pop() if stack else ' '
-            
-    return count
+                if board[y][x] != " ":
+                    move_list.append(board[y][x])
+            for y in range(m-1, -1, -1):
+                board[y][x] = move_list.popleft() if move_list else " "
+                
+    return answer
