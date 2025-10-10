000b16a0d334cc945320e5b26ecd0490c5f90c40
kdaehun00 <kdaehun00@naver.com>
2025-10-10 17:52:49 +0900

    [level 2] Title: 순위 검색, Time: 684.33 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/순위 검색.py
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.py
@@ -3,34 +3,31 @@ from itertools import combinations
 import bisect
 
 def solution(info, query):
+    answer = []
     db = defaultdict(list)
-    for i in info:
-        parts = i.split()
-        conditions = parts[:-1]
-        cost = int(parts[-1])
-        for r in range(5):
-            for comb in combinations(range(4), r):
-                temp = conditions[:]
-                for idx in comb:
-                    temp[idx] = '-'
-                key = ' '.join(temp)
-                db[key].append(cost)
-
+    stack_index = [x for x in range(4)]
+    
+    for employee in info:
+        data_list = list(employee.split())
+        stack, score = data_list[:-1], int(data_list[-1])
+        
+        for i in range(5):
+            for j in combinations(stack_index, i):
+                temp = stack.copy()
+                for change_index in j:
+                    temp[change_index] = "-"
+                db[" ".join(temp)].append(score)
+    
     for key in db:
         db[key].sort()
-
-    answer = []
-    for q in query:
-        q = q.replace(' and ', ' ')
-        parts = q.split()
-        key = ' '.join(parts[:-1])
-        cost = int(parts[-1])
-
-        if key in db:
-            costs = db[key]
-            idx = bisect.bisect_left(costs, cost)
-            answer.append(len(costs) - idx)
-        else:
-            answer.append(0)
-
+        
+    for data in query:
+        data = data.replace(" and", "")
+        find_list = list(data.split())
+        stack, score = " ".join(find_list[:-1]), int(find_list[-1])
+        
+        scores = db.get(stack, [])
+        idx = bisect.bisect_left(scores, score)
+        answer.append(len(scores) - idx)
+        
     return answer
